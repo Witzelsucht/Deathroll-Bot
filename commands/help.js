7827ba493e3c76e6bc2b0a9ec9b227d4a22bad70
Konrad <Witzelsucht@users.noreply.github.com>
2021-03-01 19:07:58 +0100

    help edit

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'help',
     descritpion: "",
     execute(message, args) {
-        message.author.send("!dr join - sign for the next game\n !dr roll (or just !dr) - make a roll.\n!dr start {max roll} {qc} - starts a game.\nThe {max roll} parameter sets the maximum value that can be drawn. Default is 10000000\nThe {qc} parameter turns on queue check, type qc to turn on.")
+        message.author.send("!dr join - sign for the next game\n !dr roll - make a roll.\n!dr start {max roll} {qc} - starts a game.\nThe {max roll} parameter sets the maximum value that can be drawn. Default is 10000000\nThe {qc} parameter turns on queue check, type qc to turn on.")
     }
 }
